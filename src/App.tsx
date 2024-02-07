7a4e92e0650053a1b343fb24951b3cb3e4da9786
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-02-07 16:36:49 -0500

    Completed task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -7,6 +8,36 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript. My name is Maanav.
             </header>
+            <header className="App-header" role="heading">
+                So here is another header
+            </header>
+            <img
+                src="https://w.wallha.com/ws/9/JYjveKLu.jpg"
+                alt="This looks cool"
+            />
+            Unordered List:
+            <ul>
+                <li>First thing</li>
+                <li>Another thing</li>
+                <li>A third item</li>
+            </ul>
+            <div>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+            </div>
+            <Container>
+                <Row>
+                    <Col>
+                        First Column
+                        <div className="Red-rectangle"></div>
+                    </Col>
+                    <Col>
+                        Second column. Tada 2-column layout
+                        <div className="Red-rectangle"></div>
+                    </Col>
+                </Row>
+            </Container>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World.
