52015fd62a2578caea333bc126490f194859f135
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-03-04 13:57:27 -0500

    Implemented TwoDice (This was tricky to implement)

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -1,16 +1,46 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-/**
- * Here is a helper function you *must* use to "roll" your die.
- * The function uses the builtin `random` function of the `Math`
- * module (which returns a random decimal between 0 up until 1) in order
- * to produce a random integer between 1 and 6 (inclusive).
- */
 export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftdie, setLeftDie] = useState<number>(1);
+    const [rightdie, setRightDie] = useState<number>(2);
+    const [result, setResult] = useState<string | null>(null);
+
+    const rollLeft = () => {
+        const newLeftDie = d6();
+        setLeftDie(newLeftDie);
+        updateResult(newLeftDie, rightdie);
+    };
+
+    const rollRight = () => {
+        const newRightDie = d6();
+        setRightDie(newRightDie);
+        updateResult(leftdie, newRightDie);
+    };
+
+    const updateResult = (left: number, right: number) => {
+        if (left === right) {
+            setResult(left !== 1 ? "Win!" : "Lose");
+        } else {
+            setResult("Lose");
+        }
+    };
+
+    return (
+        <div>
+            <div>Two Dice</div>
+            <span data-testid="leftdie">{leftdie} </span>
+            <span data-testid="rightdie"> {rightdie}</span>
+            <p></p>
+
+            <Button onClick={rollLeft}>Roll Left</Button>
+            <Button onClick={rollRight}>Roll Right</Button>
+
+            {result && <div>{result}</div>}
+        </div>
+    );
 }
