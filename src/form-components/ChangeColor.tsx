11c8b14af360c542b3683d140b575c2ee59559f8
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-03-07 21:01:51 -0500

    Implemented ChangeColor

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,47 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): JSX.Element {
+    const colors = [
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "indigo",
+        "violet",
+        "purple"
+    ];
+
+    const [selectedColor, setSelectedColor] = useState<string>(colors[0]);
+
     return (
         <div>
             <h3>Change Color</h3>
+            {colors.map((color) => (
+                <label key={color}>
+                    <Form.Check
+                        inline
+                        type="radio"
+                        name="color"
+                        label={color}
+                        value={color}
+                        checked={selectedColor === color}
+                        onChange={() => setSelectedColor(color)}
+                    />
+                </label>
+            ))}
+            <div
+                data-testid="colored-box"
+                style={{
+                    width: "200px",
+                    height: "50px",
+                    backgroundColor: selectedColor,
+                    marginTop: "10px"
+                }}
+            >
+                You color is {selectedColor}
+            </div>
         </div>
     );
 }
